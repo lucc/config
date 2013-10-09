44f6fb777bd5eaeb333cbc01b9f381d365bf9f01
Lucas Hoffmann <l-m-h@web.de>
2013-10-09 23:50:52 +0200

    autocomit

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -129,9 +129,33 @@
 
 
 ; minibuffer completition {{{1
-;(ido-save-directory-list-file "~/.emacs.d/ido.last")
-(require 'ido)
-(ido-mode t)
+;; ido makes competing buffers and finding files easier
+;; http://emacs-fu.blogspot.de/2009/02/switching-buffers.html
+;; http://www.emacswiki.org/cgi-bin/wiki/InteractivelyDoThings
+(require 'ido) 
+(ido-mode 'both) ;; for buffers and files
+(setq 
+  ido-save-directory-list-file "~/.emacs.d/cache/ido.last"
+
+  ido-ignore-buffers ;; ignore these guys
+  '("\\` " "^\*Mess" "^\*Back" ".*Completion" "^\*Ido" "^\*trace"
+
+     "^\*compilation" "^\*GTAGS" "^session\.*" "^\*")
+  ido-work-directory-list '("~/" "~/Desktop" "~/Documents" "~src")
+  ido-case-fold  t                 ; be case-insensitive
+
+  ido-enable-last-directory-history t ; remember last used dirs
+  ido-max-work-directory-list 30   ; should be enough
+  ido-max-work-file-list      50   ; remember many
+  ido-use-filename-at-point nil    ; don't use filename at point (annoying)
+  ido-use-url-at-point nil         ; don't use url at point (annoying)
+
+  ido-enable-flex-matching nil     ; don't try to be too smart
+  ido-max-prospects 8              ; don't spam my minibuffer
+  ido-confirm-unique-completion t) ; wait for RET, even with unique completion
+
+;; when using ido, the confirmation is rather annoying...
+ (setq confirm-nonexistent-file-or-buffer nil)
 ;(require 'mcomplete)
 
 
