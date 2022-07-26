3b30f76d7aecdd86b57aa93f213d5c1fc03c4cc0
Lucas Hoffmann <lucc@posteo.de>
2022-07-26 22:52:23 +0200

    Move rofi config to system repo

--- rofi/bin/sms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ $# -eq 0 ]; then
-  abq.sh . | awk '{print $1}' | grep -v '^+\?[0-9]*@'
-else
-  (term -e sms "$@" & ) >/dev/null 2>&1
-fi
--- rofi/bin/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-
-if [ "$1" = quit ]; then exit; fi
-
-
-cd ~/img
-
-ls *(.) | sed "s#.*#&\\x00image\\x1ffile://$HOME/img/&#"
